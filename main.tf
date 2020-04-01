684aa9cd98e3d38467b514fa4f8aa9e783bd83f1
neethuk92 <62416379+neethuk92@users.noreply.github.com>
2020-04-01 15:04:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,19 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "${var.prefix}-resources"
-  location = "${var.location}"
+  name     = "infrarespoc"
+  location = "northeurope"
 }
 
 # NOTE: the Name used for Redis needs to be globally unique
 resource "azurerm_redis_cache" "example" {
-  name                = "${var.prefix}-cache"
-  location            = "${azurerm_resource_group.example.location}"
+  name                = "sampleredisserver"
+  location            = "northeurope"
   resource_group_name = "${azurerm_resource_group.example.name}"
   capacity            = 2
   family              = "C"
   sku_name            = "Standard"
   enable_non_ssl_port = false
   minimum_tls_version = "1.2"
-
   redis_configuration {}
 }

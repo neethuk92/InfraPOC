f858838942d1d09d9167d2c22875c18855ea3c15
neethuk92 <62416379+neethuk92@users.noreply.github.com>
2020-04-01 15:32:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
 provider "azurerm" {
+  subscription_id = "ebe05484-9894-423e-a6bd-a1ccb1aa6180"
+  client_id       = "2fcf3a5f-3f70-4570-9601-e7dcb5031c10"
+  client_secret   = "dMEm9=co0MMJY9Cr4bTIGBNgVxn]cG::"
+  tenant_id       ="87ebd5e1-02c3-4bb9-8e8b-65ef3bc9682d"
   features {}
 }
 
